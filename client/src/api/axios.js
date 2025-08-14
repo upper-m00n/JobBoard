b7af212f9ceafd8161ff65cb5205fff04b6fd297
Ashutosh Sharma <mr.proashutosh@gmail.com>
2025-08-14 16:41:30 +0530

    changed backend server

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL:'https://jobboard-production-08c3.up.railway.app/api',
+    baseURL:'https://jobboard-ztsu.onrender.com/api',
 })
 
 API.interceptors.request.use((req)=>{
